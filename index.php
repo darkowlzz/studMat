ea92a5f9474abda0a2878822c2625aac5dcd15fb
Sunny <indiasuny000@gmail.com>
2012-02-25 23:01:14 -0600

    bottom bug fixed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
-		<title>Studymaterial Downloads</title>
+		<title>StudMat</title>
 	</head>
 <body>
 
